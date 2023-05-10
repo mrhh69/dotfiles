2ce719e94f230cda6ae6d1d9ec328ba81b4bc190
Max Heuer <122405954+mrhh69@users.noreply.github.com>
2023-05-09 21:11:58 -0500

    updated fish/kitty configs
    * and added neofetch config

--- fish/config.fish
+++ fish/config.fish
@@ -11,12 +11,17 @@ if status is-interactive
 	fish_add_path "/opt/homebrew/opt/llvm/bin"
 	alias python3=/opt/homebrew/bin/python3
 
-	set -g fish_greeting "$(date): good morning!$(printf "\n")$(neofetch)$(printf "\n")"
-
 
 	set fish_color_host green
 	set fish_color_user green
 	set fish_color_cwd blue
 
 
+end
+
+
+function fish_greeting
+	# echo "Good day!"
+	# echo The time is (set_color yellow; date +%T; set_color normal)
+	neofetch --ascii_distro macOS_small --colors 2 fg fg 1 fg 4
 end
