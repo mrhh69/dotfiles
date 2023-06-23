4dd8bcf85acb3fec5a04a20a76a42e623eed9c5f
mrhh69 <122405954+mrhh69@users.noreply.github.com>
2023-06-23 15:05:28 +0000

    removed fish and kitty

--- fish/functions/fish_prompt.fish
+++
@@ -1,7 +0,0 @@
-function fish_prompt
-
-    printf '%s%s@%s%s %s%s $%s ' \
-        (set_color $fish_color_user) $USER \
-        (set_color $fish_color_host) "mbp" \
-        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
-end
