34bdfb885eb5090ddd1c21ca5a3223971cf5decb
mrhh69 <122405954+mrhh69@users.noreply.github.com>
2023-07-18 20:01:44 -0500

    fish: add fish configs (and neofetch)

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,16 @@
 status -l ; and test -f /etc/profile.env ; and source /etc/profile.env
+if status is-interactive
+#	fish_add_path "EXAMPLE PATH"
+
+
+	set fish_color_host green
+	set fish_color_user green
+	set fish_color_cwd blue
+end
+
+
+function fish_greeting
+	# echo "Good day!"
+	# echo The time is (set_color yellow; date +%T; set_color normal)
+	neofetch #--ascii_distro gentoo_small
+end
